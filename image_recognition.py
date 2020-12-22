de70b196870d872547917d476f7796c235a46b10
StanPeter <stanislav.petr7@gmail.com>
2020-12-22 22:58:47 +0100

    optimalized

--- image_recognition.py
+++ image_recognition.py
@@ -6,9 +6,9 @@ execution_path = os.getcwd()
 
 prediction = ImagePrediction()
 prediction.setModelTypeAsResNet()
-prediction.setModelPath(os.path.join(execution_path, "resnet50_weights_tf_dim_ordering_tf_kernels.h5"))
+prediction.setModelPath(os.path.join(execution_path, "resnet50_weights_tf_dim_ordering_tf_kernels.h5")) # needs to be downloaded
 prediction.loadModel()
 
-predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "giraffe.jpg"), result_count=5 )
+predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "images/giraffe.jpg"), result_count=5 )
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
 Some machine learning basics I have practised
 
 Its not a project at all, more like a backup for myself.
+
+
+"resnet50_weights_tf_dim_ordering_tf_kernels.h5" needs to be downloaded
